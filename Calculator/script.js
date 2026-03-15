c8b8535a5c82a5d139c5152754a8d5526732ed0b
ZayThiha <109571154+ZayThihaAung@users.noreply.github.com>
2026-03-15 14:20:45 +0630

    Update script.js
    Fix the calculation error and added some features.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,12 +1,28 @@
 const resultTag = document.querySelector('.result');
 
 let calculation = '';
+let index = 0;
 function calculate(item) {
-	if (item === '') {
-		calculation = eval(item);
+	if (item === 'clear') {
+		calculation = '';
 		resultTag.innerHTML = calculation;
-		return
+		return;
 	}
-	calculation = item;
-  resultTag.innerHTML += calculation;
+	if (item === ' = ') {
+		try {
+			calculation = eval(calculation) + '';
+			resultTag.innerHTML = calculation;
+			index++;
+		} catch (error) {			
+			resultTag.innerHTML = 'Error';
+			calculation = '';
+		}
+		return;
+	} 
+	if (index > 0) {
+		calculation = '';
+		index = 0;
+	}
+	calculation += item;
+  resultTag.innerHTML = calculation;
 }
