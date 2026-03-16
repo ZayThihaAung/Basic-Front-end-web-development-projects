bdca969185248299ff55461eefd88e841da5510e
ZayThiha <109571154+ZayThihaAung@users.noreply.github.com>
2026-03-16 14:34:45 +0630

    Add files via upload

---
+++ Amazon/scripts/checkout.js
@@ -0,0 +1,7 @@
+import renderOrderSummary from "./Checkout/orderSummary.js";
+import renderPaymentSummary from "./Checkout/paymentSummary.js";
+import { renderCheckoutHeader } from "./Checkout/checkHeader.js";
+
+renderCheckoutHeader();
+renderOrderSummary();
+renderPaymentSummary();
