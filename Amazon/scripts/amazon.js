2f3afb2e5f307cfacf39a7dae5565b17da0032bd
ZayThiha <109571154+ZayThihaAung@users.noreply.github.com>
2026-03-17 14:36:22 +0630

    Update amazon.js
    Added seach features to the project.

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -1,13 +1,14 @@
-import { cart, addToCart } from "../data/cart.js"; // rememeber type at the top of the file when you use import 
-import { products } from "../data/products.js"; // can be only work with live sever 
-// can be renamed like cart as myCart
-import { formatCurrency } from "./utils/money.js";
-let productsHtml = '';
-const cartQuantityTag =  document.querySelector('.js-cart-quantity');
 // Main Idea of JS
 // 1) Save the data
 // 2) Generate the HTML
 // 3) Make it interactive
+import { cart, addToCart } from "../data/cart.js";  
+import { products } from "../data/products.js"; 
+import { formatCurrency } from "./utils/money.js";
+let productsHtml = '';
+const cartQuantityTag = document.querySelector('.js-cart-quantity');
+const inputSearch = document.querySelector('.search-bar');
+
 products.forEach(product => {
   productsHtml += `
     <div class="product-container">
@@ -85,7 +86,7 @@ function updateCartQuantity(addedCartQuantity) {
 document.querySelectorAll('.js-add-to-cart')
   .forEach(buttons => {
     buttons.addEventListener('click', () => { // this is the name converted from HTML product-id
-      const productId = buttons.dataset.productId; // The way can get data attribute from HTML
+      const productId = buttons.dataset.productId; // get data attribute from HTML
       const selectedProduct = document.querySelector(`.js-quantity-selector-${productId}`).value;
       const addedCartQuantity = document.querySelector(`.js-added-to-cart-${productId}`);
 
@@ -94,3 +95,66 @@ document.querySelectorAll('.js-add-to-cart')
       updateCartQuantity(addedCartQuantity);
     })
   })
+
+// For Searching the product
+inputSearch.addEventListener('input', (event) => {
+  const searchText = event.target.value.toLowerCase();
+  const filteredProducts = products.filter(product => {
+    return product.name.toLowerCase().includes(searchText);
+  })
+  productsHtml = '';
+  filteredProducts.forEach(product => {
+    productsHtml += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
+        </div>
+
+        <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price"> 
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+  })
+  document.querySelector('.js-products-grid').innerHTML = productsHtml;
+})
